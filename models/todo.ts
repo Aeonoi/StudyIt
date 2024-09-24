ed862c367ced6193f9c08336769ea17eee50a158
Aeonoi <dzhou527@gmail.com>
2024-09-23 20:21:21 -0400

    feat(todo): create model for todo
    closed #12

--- models/todo.ts
+++ models/todo.ts
@@ -1,8 +1,33 @@
 import mongoose from "mongoose";
+import type { Document } from "mongoose";
 
-const todo = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
+export interface ITodo extends Document {
+  _id: string;
+  name: string;
+  createdDate: Date;
+  dueDate: Date;
+  priority: number;
+  content: string;
+}
+
+export const TodoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+  },
+  dueDate: {
+    type: Date,
+  },
+  priority: {
+    type: Number,
+  },
+  content: {
+    type: String,
+  },
 });
+
+const Todo = mongoose.models.Todo || mongoose.model<ITodo>("Todo", TodoSchema);
+export default Todo;
