9a161bc8eb1e72cc75bff017475bb3819d5896f3
Aeonoi <dzhou527@gmail.com>
2024-10-02 21:31:34 -0400

    feat(chart): added chart for showcasing total focus time depending on day, month, year (if possible)
    19

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 "use client";
+import ChartLineTotalFocus from "@/components/ChartLineTotalFocus";
 import ChartTotalFocus from "@/components/ChartTotalFocus";
 import Navbar from "@/components/Navbar";
 import NonChartCards from "@/components/NonChartCards";
+import { getTotalFocusTime } from "@/lib/dashboard";
+import { useEffect } from "react";
 export default function Home() {
+  useEffect(() => {
+    const test = async () => {
+      await getTotalFocusTime();
+    };
+    test();
+  });
   return (
     <>
       <Navbar />
@@ -11,6 +20,7 @@ export default function Home() {
       </div>
       <div className="grid grid-cols-3 items-center justify-center p-8 pb-20 gap-10 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <ChartTotalFocus />
+        <ChartLineTotalFocus />
       </div>
     </>
   );
